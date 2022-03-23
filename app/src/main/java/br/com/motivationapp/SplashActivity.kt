d251e42af5814048f7346de4b497640dff16235a
Gabriel Palhares <devpalhares@gmail.com>
2022-03-22 22:09:52 -0300

    first commit, motivation app

---
+++ app/src/main/java/br/com/motivationapp/SplashActivity.kt
@@ -0,0 +1,16 @@
+package br.com.motivationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (supportActionBar != null)
+        {
+        supportActionBar!!.hide()
+        }
+    }
+}
